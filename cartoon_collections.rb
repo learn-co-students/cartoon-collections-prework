2abaed8d1d691214e23c413dddcb14c2cc720bd5
github username <github email address>
2017-03-15 10:11:27 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,20 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese(array)
+def find_the_cheese(array) #defining method arguement of array
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?("cheddar")
-    return "cheddar"
+  #variable cheese_types includes "cheddar" "gouda" and "camembert"
+  cheese_types.each do |string|
+    #check for each element of cheese_types. string var will be
+    #changed for each element in cheese_types
+    #first loop, "cheddar" second loop, "gouda", third loop "camembert"
+    if array.include?(string)
+      #if the main array includes any of the cheese_types strings it just looped thru
+      return string
+    #return whichever cheese_type string came first because as soon as it
+    #hits the thing you were looking for, return exits out of the function
+    end
   end
+  return nil
+  #if the loop completes w/o finding anything, then return "nil"
 end
