857bce370d91cc0c493c3f735f9c53842999546e
Ariel Herman <aeh.herman@gmail.com>
2017-08-02 11:47:28 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index { |name, idx| puts "#{idx + 1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |elem| elem.capitalize! }
+  planeteer_calls.collect { |elem| elem + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  # .select or .count
+  calls.count { |x| x.length > 4 } > 0
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings)
+  # look through all the strings. If any is included in cheese_types, return that cheese in a str format
   cheese_types = ["cheddar", "gouda", "camembert"]
+    if strings.include?("cheddar")
+      return "cheddar"
+    elsif strings.include?("gouda")
+      return "gouda"
+    elsif strings.include?("camembert")
+      return "camembert"
+    else return nil
+  end
+  #string.include? {|x|}
 end
