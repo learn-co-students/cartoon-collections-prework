a2864b3f5dcc20da71ae726cd45d264b44c4c3c8
gene-yoo <gene.y.yoo@gmail.com>
2017-08-24 15:19:02 -0400

    Lab complete.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index|
+    puts "#{index+1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |call|
+    call.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  # iterate over each cheese type
+  # conditional to match cheese type with food array items
+  # return matching food item; nil if otherwise
+
+  cheese_types.each do |cheese|
+    if foods.include?(cheese)
+      return cheese
+    end
+  end
+  nil
 end
