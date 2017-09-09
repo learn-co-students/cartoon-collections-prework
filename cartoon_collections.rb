702ee25b1373ec518e95aa587748c230b046bffb
Paul Kristapovich <paulkristapovich@gmail.com>
2017-09-09 22:17:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do |x,index|
+    puts"#{index+1}. #{x}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect do |x|
+    x.capitalize!
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? do |x|
+    x.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
