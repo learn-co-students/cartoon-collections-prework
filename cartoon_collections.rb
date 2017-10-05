50a191b7caa7ad487ce0a76fc609144e2b54d4b6
github username <github email address>
2017-10-04 21:52:03 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,90 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarfString = ""
+  for index in 1..dwarves.length do
+    dwarfString << "#{index}. #{dwarves[index - 1]} "
+  end
+  puts dwarfString
+end
+def summon_captain_planet(array)# code an argument here
+  newArray = []
+  array.each do |item|
+    newArray.push(item.capitalize<<"!")
+  end
+  return newArray
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.each do |item|
+    if item.length > 4
+      return true
+    end
+  end
+  return false
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.each do |food|
+    cheese_types.each do |cheese|
+      if food == cheese
+        return food
+      end
+    end
+  end
+  return nil
+
 end
+
+
+#function dwarfRollCall(dwarves)
+#{
+#  var dwarfString = '';
+#  for(var i = 1; i <= dwarves.length; i++)
+#  {
+#    dwarfString += `${i}. ${dwarves[i - 1]} `;
+#  }
+
+#  return dwarfString;
+#}
+
+#function summonCaptainPlanet(planeteerCalls)
+#{
+#  var capitalArray = [];
+#  for(var i = 0; i < planeteerCalls.length; i++)
+#  {
+#    capitalArray[i] = `${planeteerCalls[i].toUpperCase()}!`;
+
+#  }
+
+#  return capitalArray;
+#}
+
+#function longPlaneteerCalls(words)
+#{
+#  for(var i = 0; i < words.length; i++)
+#  {
+#    if(words[i].length > 4)
+#    {
+#      return true;
+#    }
+#  }
+#  return false;
+#}
+
+#function findTheCheese (foods)
+#{
+#  //cheddar, gouda, and camembert
+#  var cheeseArray = ['cheddar', `gouda`, `camembert`];
+#  for(var i = 0; i < foods.length; i++)
+#  {
+#    for(var o = 0; o < cheeseArray.length; o++)
+#    {
+#        if(foods[i] === cheeseArray[o])
+#        {
+#            return foods[i];
+#        }
+#    }
+#  }
+#  return "no cheese!";
+#}
