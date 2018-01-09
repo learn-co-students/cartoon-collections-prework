cdab10e5743e01549148740d707d851fc44f7c8b
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-09 04:01:25 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each_with_index do |element, index|
+    puts "#{index + 1}. #{element} "
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map do |element|
+    element.capitalize
+  end
 end
 
 def long_planeteer_calls# code an argument here
