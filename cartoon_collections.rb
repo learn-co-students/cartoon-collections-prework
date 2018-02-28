3790878c25d1a900d697dede036ee3d0c55af09e
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 21:09:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each_with_index do |value, index|
+    puts "#{index + 1}. #{value}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map { |w| w.capitalize << "!"}
+  end
+
+def long_planeteer_calls(arr)
+  arr.any? do |word|
+  word.length > 4
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find do |w|
+    cheese_types.include?(w)
+  end
 end
