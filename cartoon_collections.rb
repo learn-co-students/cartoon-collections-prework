2b77aea7a583c2b75d6bcd12393a94270d8c7386
LaneMiller Miller <chris.lane.miller@gmail.com>
2018-03-20 15:37:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 1
+  dwarves.collect do |name|
+    puts "#{i}. #{name}"
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect {|word| word.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.include?()
 end
 
 def find_the_cheese# code an argument here
