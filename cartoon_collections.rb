e0e75ac6dc5821955ac7761cb4154a82e8ee769c
JohnZBurnett <tuninator@gmail.com>
2018-04-11 19:03:04 -0400

    Wrote enumerable methods.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_list)
+  dwarf_list.each_with_index do |dwarf, idx|
+    puts (idx + 1).to_s + '. ' + dwarf
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  emphatic_arr = planeteers.collect do |name|
+    name.capitalize + '!'
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_list)
+  if call_list.find { |item| item.length > 4 }
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(item_list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  item_list.each do |item|
+    if cheese_types.include?(item)
+      return item
+    end
+  end
+  return nil 
 end
