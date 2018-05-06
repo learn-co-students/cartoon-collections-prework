10e5cafc7e5af4d5e51df7b91776a193a5cb4dd3
Anthony Lam <al3101@nyu.edu>
2018-05-06 18:56:40 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.each_with_index do |name, index|
+    index += 1 
+    puts "#{index}. #{name}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers) 
+  planeteers.collect do |planteer| 
+    planteer.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls) 
+  calls.any? do |call|
+    call.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.each do |snack|
+    if cheese_types.include?(snack)
+      return snack 
+    end
+  end
+  return nil 
 end
