22f1d232dd07afdb1f096f9054c940c6f8809ed1
github username <github email address>
2018-05-08 16:25:43 -0400

    Implemented all the cartoon methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  counter = 0
+  dwarves.each do |dwarve|
+    puts "#{counter+=1}. #{dwarve}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.collect { |planeteer| "#{planeteer.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  short_words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(possible_cheeses)
+  the_cheese = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if the_cheese == nil
+      the_cheese = possible_cheeses.find { |possible| possible == cheese }
+    end
+  end
+  the_cheese
 end
