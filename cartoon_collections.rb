1d354701073996475b495377f18ce5f16bf4a8f2
jchamberlain909 <jchamberlain909@gmail.com>
2018-05-10 11:02:01 -0400

    implemented methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (arr)
   # Your code here
+  arr.each_with_index do |dwarf, i|
+    puts "/#{i+1}.*#{dwarf}/"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)
   # Your code here
+  arr.collect do |e|
+    e=e.capitalize+"!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arr)
   # Your code here
+  arr.any? { |e| e.length>4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |e|
+    if(arr.include?(e))
+      return arr.find {|el| el==e}
+    end
+  end
+  return nil
 end
