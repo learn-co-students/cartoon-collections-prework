30013c065edeae3452e79a72a5dfa0b0759ff309
Stephen F <stephen@farver.io>
2018-05-28 04:17:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index {|el, i| puts "#{i+1}. #{el}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|el| "#{el.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  short_flag = false
+  array.each {|word| word.length > 4 ? short_flag = true : nil}
+  short_flag
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(string)
+  output = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
+  string.find do |el|
+    if cheese_types.include? el 
+      output = el
+    end
+  end
+  
+  output
 end
