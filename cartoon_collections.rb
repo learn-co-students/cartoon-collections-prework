a3e2c3e59bb944684cc8cbd9af93abefb3af3f20
Bryan Teng <bteng1226@gmail.com>
2018-06-24 18:40:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each{|x| puts(x)}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
 end
 
