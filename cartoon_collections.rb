50650be38a9deb0ccf344c61a93e3de5d5ad5c17
yoshinator <yoanante@gmail.com>
2018-09-20 19:13:00 -0400

    Captain Planet, he's our hero, he's going to take pollution down to zero

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) {|dwarve, index| puts "#{index} #{dwarve}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(call)
+  call.collect {|saying| saying.capitalize<<"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(word_list)
+  word_list.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.find {|item| cheese_types.include?(item)}
 end
