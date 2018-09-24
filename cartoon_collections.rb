c4c80a4f468987fd77ed5cf048449db98d0ffad2
thompson-ad <thompson_ad@outlook.com>
2018-09-24 05:32:57 +0100

    cartoon final

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  #It should then print out each name, in number order, using puts. 
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.map do |values|
+    values << "!"
+    values.capitalize
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+  def long_planeteer_calls(array)
+    array.any? do |word| 
+      word.length > 4 
+    end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese| 
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    end
 end
