dcc2391cce2310f5536dfcacc454fbfe435dc295
Joe Huntington <jhuntington@tutanota.com>
2018-09-24 17:22:37 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  count = 1
+  dwarves.each do |dwarf|
+    puts "/#{count}.*#{dwarf}/"
+    count += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planets)
+  count = 0
+  planets.collect do |planet|
+    planet.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_planeteer_calls)
+  long_planeteer_calls.any? do |planeteer|
+    planeteer.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  result = cheese.select{|x| cheese_types.include?(x)}
+  if(result.size == 0)
+    result = nil
+  else
+    result.join
+  end
 end
