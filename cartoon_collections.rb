d3152f8a54f88a6a788cb256a6069d2037945e5f
heather sterman <heather.sterman531@gmail.com>
2018-10-15 21:03:37 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
 def roll_call_dwarves# code an argument here
-  # Your code here
+  dwarves = ["Happy", "Doc", "Dopey", "Sneezy"]
+  puts 
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+  planeteer_calls = "earth", "wind", "fire", "water", "heart"]
+  planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize }
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+  long_words = ["industrious", "excellent", "super"]
+  
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+array.any? {|cheddar| cheese_types.include?(cheddar)}
 end
