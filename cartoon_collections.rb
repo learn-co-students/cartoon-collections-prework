4c7ef1281f5304e2be16453020875896b4759347
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-15 22:40:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(names)
+  names.each_with_index do |dwarf, index|
+    puts "#{index + 1} #{dwarf}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |element| 
+    element = element.capitalize
+    element = element + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |element|
+    length = element.length
+    if length > 4
+      return true 
+    end 
+  end
+  false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |element|
+    if cheese_types.include?(element)
+      return element
+    end 
+  end 
+  nil
 end
