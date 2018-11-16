73cdbea39f00be9d69a3999e5ff8879d0ee318eb
lumrachele <rlum894@gmail.com>
2018-11-16 11:50:01 -0500

    practice using different enumerator methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (dwarves)
+  dwarves.each_with_index { |dwarf, index|
+    puts "#{index+1}. #{dwarf}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (produce)
+  produce.map! {|item| item.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (words)
+  words.any? do |given_word|
+    given_word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  snacks.find do |snack|
+    if cheese_types.include?(snack)
+      snack
+    end
+  end
 end
