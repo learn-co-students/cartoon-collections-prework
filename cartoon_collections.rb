753dd283b3289a707e2f7ade2ddc79bd7210b3b6
github username <rasha.baig@gmail.com>
2019-01-09 13:50:52 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  empty_Array = []
+    i = 0
+    while i < dwarves.length
+  empty_Array << "#{i+1}. #{dwarves[i]}"
+  i += 1
+  puts empty_Array
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(planteer_calls)
+  empty_Array = []
+  i = 0
+  while i < planteer_calls.length
+  puts empty_Array << "#{planteer_calls[i].capitalize}!"
+  i += 1
+end
+empty_Array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(snack)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snack.find do |item|
+    cheese_types.include?(item)
+end
 end
