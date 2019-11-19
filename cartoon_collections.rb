6df7eeaeca92363bf84f86108832dbb10cef51ca
dagonyclark <sean@dagony-clark.com>
2019-11-19 09:08:47 -0500

    Revert "Done."
    This reverts commit 5c42e4e2b2c7e268d64b77a7f8d7ed3d18495e14.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,23 +1,16 @@
-def roll_call_dwarves(names)
-  names.each_with_index do |name, index|
-    puts "#{index + 1}. #{name}"
-  end
+def roll_call_dwarves# code an argument here
+  # Your code here
 end
 
-def summon_captain_planet(calls)
-  calls.collect do |call|
-    call.capitalize! << "!"
-  end
+def summon_captain_planet# code an argument here
+  # Your code here
 end
 
-def long_planeteer_calls(calls)
-  calls.any? do |call|
-    call.length > 4
-  end
+def long_planeteer_calls# code an argument here
+  # Your code here
 end
 
-def find_the_cheese(array)
-  array.find do |cheese|
-    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
-  end
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
 end
